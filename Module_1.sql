77c3e45d9247b9d9efba2926b8c4483d6a6a8fa9
RashmiSingh23 <154654797+RashmiSingh23@users.noreply.github.com>
2023-12-24 12:46:33 +0530

    Add files via upload

---
+++ Module_1.sql
@@ -0,0 +1,53 @@
+/*
+-- Query: select last_name,job_id,salary from employees
+where job_id IN ('SA_REP','ST_CLERK') AND salary NOT IN (2500,3500,5000)
+LIMIT 0, 1000
+
+-- Date: 2023-12-24 12:44
+*/
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Nayer','ST_CLERK',3200.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Mikkilineni','ST_CLERK',2700.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Landry','ST_CLERK',2400.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Markle','ST_CLERK',2200.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Bissot','ST_CLERK',3300.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Atkinson','ST_CLERK',2800.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Olson','ST_CLERK',2100.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Mallin','ST_CLERK',3300.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Rogers','ST_CLERK',2900.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Gee','ST_CLERK',2400.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Philtanker','ST_CLERK',2200.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Ladwig','ST_CLERK',3600.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Stiles','ST_CLERK',3200.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Seo','ST_CLERK',2700.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Davies','ST_CLERK',3100.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Matos','ST_CLERK',2600.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Tucker','SA_REP',10000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Bernstein','SA_REP',9500.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Hall','SA_REP',9000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Olsen','SA_REP',8000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Cambrault','SA_REP',7500.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Tuvault','SA_REP',7000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('King','SA_REP',10000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Sully','SA_REP',9500.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('McEwen','SA_REP',9000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Smith','SA_REP',8000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Doran','SA_REP',7500.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Sewall','SA_REP',7000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Vishney','SA_REP',10500.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Greene','SA_REP',9500.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Marvins','SA_REP',7200.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Lee','SA_REP',6800.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Ande','SA_REP',6400.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Banda','SA_REP',6200.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Ozer','SA_REP',11500.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Bloom','SA_REP',10000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Fox','SA_REP',9600.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Smith','SA_REP',7400.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Bates','SA_REP',7300.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Kumar','SA_REP',6100.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Abel','SA_REP',11000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Hutton','SA_REP',8800.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Taylor','SA_REP',8600.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Livingston','SA_REP',8400.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Grant','SA_REP',7000.00);
+INSERT INTO `` (`last_name`,`job_id`,`salary`) VALUES ('Johnson','SA_REP',6200.00);
